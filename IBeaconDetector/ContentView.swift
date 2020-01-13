0e8826485240bddcd2f5e9685953bc0bcb818324
Jian Ma <jianquanma@gmail.com>
2020-01-13 18:57:45 -0500

    Initial Commit

---
+++ IBeaconDetector/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IBeaconDetector
+//
+//  Created by Jian Ma on 1/13/20.
+//  Copyright © 2020 Jian Ma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
