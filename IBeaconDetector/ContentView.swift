cc63c30a605fef1f418f1ad7b15c086d5239b7c7
Jian Ma <jianquanma@gmail.com>
2020-01-14 19:24:32 -0500

    set up basic text and created cllocationmanager

--- IBeaconDetector/ContentView.swift
+++ IBeaconDetector/ContentView.swift
@@ -5,12 +5,43 @@
 //  Created by Jian Ma on 1/13/20.
 //  Copyright © 2020 Jian Ma. All rights reserved.
 //
-
+import Combine
+import CoreLocation
 import SwiftUI
 
+
+class BeaconDetector: NSObject, CLLocationManagerDelegate, ObservableObject{
+    
+    @Published var didChange = PassthroughSubject<Void, Never>()
+    var locationManager: CLLocationManager?
+    var lastDistance = CLProximity.unknown
+    
+    override init(){
+        super.init()
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse{
+            
+        }
+    }
+}
+
 struct ContentView: View {
+    
+    
+    
+    
     var body: some View {
         Text("UNKNOWN!")
+            .font(Font.system(.title, design: .rounded))
+            .frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, minHeight: 0, idealHeight: .infinity, maxHeight: .infinity, alignment: .center)
+            .background(Color.gray)
+           .edgesIgnoringSafeArea(.all)
     }
 }
 
